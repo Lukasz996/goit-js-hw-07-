33190d4db479290077803f99247a849adcad777d
Łukasz <lukaszkogut1996@gmail.com>
2023-11-14 20:08:45 +0100

    02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+    const imagesHTML = galleryItems.map((item) => {
+        return `<a class='gallery__item' href='${item.original}'>
+        <img class='gallery__image' src='${item.preview}' alt='${item.description}' />
+         </a>`;
+      });
+      gallery.insertAdjacentHTML('beforeend', imagesHTML.join(''));
+    
+      const lightbox = new SimpleLightbox('.gallery a', {
+        loop: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+        docClose: true,
+        alertErrorMessage: 'Image not found, next image will be loaded'
+      });
+
